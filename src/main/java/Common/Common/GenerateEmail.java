3ae1af5bccd54147cf43e77c3c5bbd4770d30860
khoaLGG <ndk1031999@gmail.com>
2021-07-04 20:23:48 +0700

    tc07 generate email function

---
+++ src/main/java/Common/Common/GenerateEmail.java
@@ -0,0 +1,33 @@
+package Common.Common;
+
+
+import java.util.Random;
+
+
+public class GenerateEmail {
+
+    private static final String alpha = "abcdefghijklmnopqrstwvxyzABCDEFGHIJKLMNOPQRSTWVXYZ";
+    private static final String digits = "0123456789";
+
+
+    public static String GenRandomString() {
+        int rdText;
+        int rdNumber;
+        String randomString = "";
+        Random rd = new Random();
+        for (int i = 0; i < 4; i++) {
+            rdText = rd.nextInt(alpha.length() - 1);
+            randomString+=(Character.toString(alpha.charAt(rdText)));
+            rdNumber = rd.nextInt(digits.length() - 1);
+            randomString+=(Character.toString(digits.charAt(rdNumber)));
+        }
+        return randomString ;
+    }
+
+    public static void main(String a[]) {
+
+        System.out.println(GenRandomString() + "@"+GenRandomString()+".com");
+    }
+}
+
+
