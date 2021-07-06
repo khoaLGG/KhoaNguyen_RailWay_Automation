9fcd3f660526d0ac68500fbac556984292c5c119
khoaLGG <ndk1031999@gmail.com>
2021-07-06 11:26:21 +0700

    updated generate email, add generate suffix email func , tc07- 08

--- src/main/java/Common/Constant/Constant.java
+++ src/main/java/Common/Constant/Constant.java
@@ -8,6 +8,7 @@ public class Constant {
     public static final String RAILWAY_URL = "http://www.railway2.somee.com/Page/HomePage.cshtml";
     public static final String USERNAME = "khoa@logigear.com";
     public static final String PASSWORD = "123456789";
+    public static final String PID = "123456789";
     public static final String INVALID_PASSWORD = "1";
     public static final String MSG_CHANGE_PASSWORD_SUCCESS = "Your password has been updated!";
     public static String MSG_WELCOME_USER = "Welcome %s";
