138d52e730c71443ac887ae16778e032d1ba1d2d
khoaLGG <ndk1031999@gmail.com>
2021-07-07 15:59:23 +0700

    updated tc10 ,add Reprot HTML file

--- src/test/java/Testcases/Railway/BookTicketTest.java
+++ src/test/java/Testcases/Railway/BookTicketTest.java
@@ -2,6 +2,10 @@ package Testcases.Railway;
 
 import Common.Common.GetDateToday;
 import Common.Constant.Constant;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -9,10 +13,20 @@ public class BookTicketTest extends TestBase {
 
     GetDateToday getDateToday = new GetDateToday();
 
+
     @Test(description = "TC04 - Login page displays when un-logged User clicks on Book ticket tab")
     public void TC04() {
+        ExtentReports extent = new ExtentReports();
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+        spark.config().setTheme(Theme.DARK);
+        spark.config().setDocumentTitle("MyReport");
+        spark.config().setReportName("Extent Report");
+        extent.attachReporter(spark);
+        ExtentTest test = extent.createTest("Login page displays when un-logged User clicks on Book ticket tab");
         generalPage.goToBookTicket();
         Assert.assertTrue(loginPage.isAtLoginPage(),"Login page don't displayed");
+        test.pass("Login page displayed");
+        extent.flush();
     }
     @Test(description = "TC14 - User can book 1 ticket at a time")
     public void TC14(){
