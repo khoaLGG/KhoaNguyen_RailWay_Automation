0feb08b232097778f2071bf4aa198c9a35c3b2c4
khoaLGG <ndk1031999@gmail.com>
2021-07-07 17:36:50 +0700

    test tc14 - inprogress(50%)

--- src/main/java/Common/Common/GetDateToday.java
+++ src/main/java/Common/Common/GetDateToday.java
@@ -7,8 +7,8 @@ import java.util.Calendar;
 
 public class GetDateToday {
 
-    public  String getDate(){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+    public String getDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy");
 
         Calendar c1 = Calendar.getInstance();
 
@@ -16,14 +16,18 @@ public class GetDateToday {
 
         Date date = Date.valueOf(today);
         c1.setTime(date);
-        c1.roll(Calendar.DATE, 3);
+        c1.roll(Calendar.DATE, 5);
         String result =  dateFormat.format(c1.getTime());
         System.out.println(result);
+        result = result.replace("-","/");
+        System.out.println(result);
+
         return result;
     }
 
     public static void main(String[] args) {
+        GetDateToday a = new GetDateToday();
+        System.out.println("//select[@name='Date']//option[text()=\""+a.getDate()+"\"]");
 
     }
-
 }
--- src/main/java/PageObjects/Railway/BookTicketPage.java
+++ src/main/java/PageObjects/Railway/BookTicketPage.java
@@ -1,5 +1,6 @@
 package PageObjects.Railway;
 
+import Common.Common.GetDateToday;
 import Common.Common.ScrollIntoView;
 import Common.Constant.Constant;
 import org.openqa.selenium.By;
@@ -9,24 +10,31 @@ import org.openqa.selenium.support.ui.Select;
 
 public class BookTicketPage {
 
-    ScrollIntoView scrollIntoView = new ScrollIntoView();
+
+    GetDateToday getDateToday = new GetDateToday();
 
     // Locators
-    private final By departDateDropdown = By.xpath("//select");
+    private final By departDateDropdown = By.xpath("//form[@method='post']//fieldset//ol//li//select[@name=\"Date\"]");
 
     // Elements
     public WebElement getDepartDate() {
-        return Constant.WEBDRIVER.findElement(By.xpath("//select[@name='Date']"));
+        return Constant.WEBDRIVER.findElement(By
+                .xpath("//form[@method='post']//fieldset//ol//li//select[@name=\"Date\"]"));
 
     }
 
     // Methods
     public void chooseDepartDate() {
         this.getDepartDate().click();
-        Constant.WEBDRIVER.findElement(By.xpath("//select[@name='Date']//option[text()='7/11/2021']")).click();
+        Constant.WEBDRIVER.findElement(
+                By.xpath("//select[@name='Date']//option[text()=\""+getDateToday.getDate()+"\"]"))
+                .click();
+        /*Select d = new Select(Constant.WEBDRIVER.findElement(By
+                .xpath("//form//li[1]//select")));
+        d.selectByVisibleText("7/11/2021");*/
     }
 
     /* public void selectDepartDate(String departDate){
         this.getDepartDate().selectByVisibleText(departDate);
-    }*/
+    }*///"+getDateToday.getDate()+"
 }
